5a926bbdea1e6c80ae06a364501b7e58a35fa213
Chris Edgemon <chrisedgemon@gmail.com>
2014-08-20 20:58:56 +0000

    Work on build instructions

--- install/s2.sh
+++ install/s2.sh
@@ -1,4 +1,5 @@
-sudo apt-get install python-pip python-dev postgresql-server-dev-all postgresql-9.3
+sudo apt-get install python-pip python-dev postgresql-server-dev-all 
+sudo apt-get install postgresql
 
 # Geodjango
 # sudo apt-get install binutils libproj-dev gdal-bin libxml2-dev
@@ -9,19 +10,20 @@ createuser -s chris
 createuser -s soccerstats
 logout
 
-# Add DEBUG, PROJECT_DIRNAME to custom_settings
-emacs custom_settings.py
-
-# Add SECRET_KEY to secret_settings.
-emacs secret_settings.py
-
 # modify  pg_hba.conf to accept local connections on "trust" (this is probably too broad).
+# could be /9.1/
 sudo emacs /etc/postgresql/9.3/main/pg_hba.conf 
 sudo service postgresql restart
 
 cd ~/soccer
 git clone https://github.com/Soccerstats/s2.git
 cd s2
-sudo pip install -r requirements.txt 
 
+# Add DEBUG, PROJECT_DIRNAME to custom_settings
+emacs custom_settings.py
+
+# Add SECRET_KEY to secret_settings.
+emacs secret_settings.py
+
+sudo pip install -r requirements.txt 
 ./build.sh
