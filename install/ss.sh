3e0337d682ca658c0ac120916233fae4fbabc805
Chris Edgemon <chrisedgemon@gmail.com>
2014-08-20 11:53:41 -0400

    update ss.sh

--- install/ss.sh
+++ install/ss.sh
@@ -3,6 +3,10 @@ sudo apt-get update
 sudo apt-get install git-core python3-pip mongodb-org emacs
 emacs .bashrc 
 # add equivalent to export PYTHONPATH=$PYTHONPATH:/home/chris/bin:/home/chris/www:/home/chris/repos:/home/chris/soccer
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+sudo apt-get install mongodb-org
+
 mkdir soccer/
 cd soccer/
 git clone https://github.com/Soccerstats/build.git
