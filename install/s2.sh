ce7e296fed907e6c4568145e4b9a51631327c084
Chris Edgemon <chrisedgemon@gmail.com>
2014-08-21 16:10:03 +0000

    Update install/s2.sh

--- install/s2.sh
+++ install/s2.sh
@@ -1,11 +1,6 @@
 # for building the website / postgres db
 
-sudo apt-get install python-pip python-dev postgresql-server-dev-all 
-sudo apt-get install postgresql
-sudo apt-get install python3-psycopg2
-
-# Geodjango
-# sudo apt-get install binutils libproj-dev gdal-bin libxml2-dev
+sudo apt-get install python-pip python-dev postgresql-server-dev-all postgresql python3-psycopg2
 
 sudo su - postgres
 
@@ -14,7 +9,7 @@ createuser -s soccerstats
 logout
 
 # modify  pg_hba.conf to accept local connections on "trust" (this is probably too broad).
-# could be /9.1/
+# local / local / trust
 sudo emacs /etc/postgresql/9.3/main/pg_hba.conf 
 sudo service postgresql restart
 
@@ -22,11 +17,8 @@ cd ~/soccer
 git clone https://github.com/SoccerstatsUS/s2.git
 cd s2
 
-# Add DEBUG, PROJECT_DIRNAME to custom_settings
-emacs custom_settings.py
+# Edit DEBUG, PROJECT_DIRNAME in custom_settings.py
+# Edit SECRET_KEY in secret_settings.py
 
-# Add SECRET_KEY to secret_settings.
-emacs secret_settings.py
-
-sudo pip install -r requirements.txt 
+sudo pip3 install -r requirements3.txt 
 ./build.sh
